c01a910fe368ae0148bd60337ce7fb5dc15a1902
Victor Santos <victor.eas19@gmail.com>
2023-10-07 00:34:56 -0300

    server.js: comments added

--- server.js
+++ server.js
@@ -1,64 +1,85 @@
+// Importing the 'net' module for server functionality
 const net = require('net');
+
+// Defining the port number (e.g., 3333)
 const PORT = 3333;
+
+// Setting the maximum number of clients allowed per server
 const MAX_CLIENTS = 4;
 
+// Creating an array named 'clients' to manage connected clients
 const clients = [];
 
-// BROADCAST FUNCTION - to handle messages from the socket
+// BROADCAST FUNCTION - Handles messages from the socket
 function broadcast(message, sender) {
-    if(message.startsWith('/ENTRAR')){
+    if (message.startsWith('/ENTRAR')) {
+        // Notify all online users that there's a new user online
         const [, username] = message.split(' ');
-        clients.forEach(socket => {
-            if (socket !== sender) socket.write(`${username} entrou no chat.`);
+        clients.forEach(clientSocket => {
+            if (clientSocket !== sender) clientSocket.write(`${username} entrou no chat.`);
         });
-    }else if (message.startsWith('/SAIR')) {
+    } else if (message.startsWith('/SAIR')) {
+        // Remove the user from the Client's Array and send messages to all other users notifying that the sender left the server
         const index = clients.indexOf(sender);
         clients.splice(index, 1);
-        clients.forEach(socket => {
-            if (socket !== sender) socket.write(`${sender.nickname} saiu no chat.`);
+        clients.forEach(clientSocket => {
+            if (clientSocket !== sender) clientSocket.write(`${sender.nickname} saiu do chat.`);
         });
-    } else if(message === '/USUARIOS'){
-        clients.forEach(socket => {
-            if (socket !== sender) sender.write(`${socket.nickname} está online.`);
-        });
+    } else if (message === '/USUARIOS') {
+        // Send a list of online users to the sender
+        if(clients.length-1 > 0){
+            clients.forEach(clientSocket => {
+                if (clientSocket !== sender) sender.write(`${clientSocket.nickname} está online.`);
+            });
+        } else{
+            sender.write('Ninguém está online.');
+        }
     } else {
-        clients.forEach(socket => {
-            if (socket !== sender) socket.write(`${sender.nickname}: ${message}`);
+        // Send the message to all other users
+        clients.forEach(clientSocket => {
+            if (clientSocket !== sender) clientSocket.write(`${sender.nickname}: ${message}`);
         });
     }
 }
 
-// Function responsable for change nicknames from online
-function changeNickname(username, socket){
-    if((clients.some)((client) => client.nickname === username)){
-        socket.write('O apelido ja está em uso. Escolha outro apelido\n');
-        //socket.end();
-    }
-    else {
+// CHANGE NICKNAME FUNCTION - Allows users to change their nicknames
+function changeNickname(newUsername, socket) {
+    // Check if the chosen username is already in use by another client
+    if (clients.some(client => client.nickname === newUsername)) {
+        socket.write('O nome de usuário já está em uso. Operação cancelada.');
+    } else {
         const index = clients.indexOf(socket);
         const oldNickname = socket.nickname;
-        socket.nickname = username;
+        socket.nickname = newUsername;
         clients.splice(index, 1, socket);
         socket.write('Nome de usuário alterado com sucesso!');
-        clients.forEach(client => {
-            if (client !== socket) client.write(`${oldNickname} mudou seu nome de usuário para ${socket.nickname}`);
-        })
+        // Notify other users about the nickname change
+        clients.forEach(clientSocket => {
+            if (clientSocket !== socket) clientSocket.write(`${oldNickname} mudou seu nome de usuário para ${socket.nickname}`);
+        });
     }
 }
 
-const server = net.createServer((socket) => {
+// Create a TCP server using the 'net' module
+const server = net.createServer(socket => {
     socket.setEncoding('utf-8');
-    if(clients.length >= MAX_CLIENTS) {
-        socket.write('O servidor atingiu o limite máximo de clientes');
+    // Check if the number of online users exceeds the server's maximum capacity
+    if (clients.length >= MAX_CLIENTS) {
+        socket.write('O servidor atingiu o número máximo de usuários. Acesso Negado');
         socket.end();
-    } else{
-        socket.on('data', (data) => {
-            if (data.startsWith('/ENTRAR')){
-                const [, username] = data.split(' ');
-                if(clients.some((client) => client.nickname === username)) {
-                    socket.write('O apelido ja está em uso. Operação cancelada\n');
-                    socket.end();
+    } else {
+        socket.on('data', data => {
+            if (data.startsWith('/ENTRAR')) {
+                let [, username] = data.split(' ');
+                if (username == 'undefined'){
+                    username = `user` + (clients.length + 1).toString();
+                }
+                // Check if there's an existing user with the same username
+                if (clients.some(client => client.nickname === username)) {
+                    socket.write('O nome de usuário já está em uso. Operação cancelada.\n');
+                    //socket.end();
                 } else {
+                    // Store the socket in the Client's Array
                     const nickname = username;
                     socket.nickname = nickname;
                     clients.push(socket);
@@ -68,26 +89,27 @@ const server = net.createServer((socket) => {
                         broadcast(`/SAIR`, socket);
                     });
                 }
-            }
-            else if (data === '/SAIR'){
-                broadcast('/SAIR', socket);
-            }
-            else if (data === '/NICK'){
-                const [, username] = data.split(' ');
-                console.log(username);
-                changeNickname(username, socket);
-            }
-            else if (data === '/USUARIOS'){
+            } else if (data.startsWith('/NICK')) {
+                // Receive a new username
+                const [, newUsername] = data.split(' ');
+                changeNickname(newUsername, socket);
+            } else {
+                // Broadcast a standard message
                 broadcast(data, socket);
             }
-            else{
-                broadcast(data, socket);
-            }
-            
         });
-    }    
+    }
+    // Handle errors to prevent them from crashing the server
+    socket.on('error', error => {
+        if (error.code === 'ECONNRESET') {
+            broadcast('/SAIR', socket);
+        } else {
+            console.error('Socket Error:', error);
+        }
+    });
 });
 
+// Start the server and listen on the specified port
 server.listen(PORT, () => {
-    console.log(`Server listening at ${PORT}`);
+    console.log(`Servidor executando na porta ${PORT}`);
 });
